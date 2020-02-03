ef40e029ee634e8ad6ae6b65ded753746298cb92
RIPScollon <scollon@signatureresearchinc.com>
2020-02-03 14:19:05 -0600

    Added form to add elements to canvas

--- interface.py
+++ interface.py
@@ -8,26 +8,33 @@ class CanvasDnD(tk.Frame):
         tk.Frame.__init__(self, master)
         canvas = tk.Canvas(self, width=256, height=256,
                            relief=tk.RIDGE, background="white", borderwidth=1)
-        self.defaultcolor = canvas.itemcget(canvas.create_text(30, 25,
-                                                               font=("Helvetica", 14), text="Item 1", tags="DnD"),
-                                            "fill")
-        canvas.create_text(75, 75,
-                           font=("Helvetica", 14), text="Item 2", tags="DnD")
-        canvas.create_text(125, 125,
-                           font=("Helvetica", 14), text="Item 3", tags="DnD")
-        canvas.create_text(175, 175,
-                           font=("Helvetica", 14), text="Item 4", tags="DnD")
-        canvas.create_text(225, 225,
-                           font=("Helvetica", 14), text="Item 5", tags="DnD")
+        self.defaultcolor = canvas.itemcget(
+            canvas.create_text(30, 25, font=("Helvetica", 14), text="Item 1", tags="DnD"), "fill")
+        canvas.create_text(75, 75, font=("Helvetica", 14), text="Item 2", tags="DnD")
+        canvas.create_text(125, 125, font=("Helvetica", 14), text="Item 3", tags="DnD")
+        canvas.create_text(175, 175, font=("Helvetica", 14), text="Item 4", tags="DnD")
+        canvas.create_text(225, 225, font=("Helvetica", 14), text="Item 5", tags="DnD")
         canvas.pack(expand=1, fill=tk.BOTH)
         canvas.tag_bind("DnD", "<ButtonPress-1>", self.down)
         canvas.tag_bind("DnD", "<ButtonRelease-1>", self.chkup)
         canvas.tag_bind("DnD", "<Enter>", self.enter)
         canvas.tag_bind("DnD", "<Leave>", self.leave)
 
+        self.canvas = canvas
+
+        tk.Label(master, text="Name", width=50, anchor='w').pack(side='top')
+        name_entry = tk.Entry(master, width=50).pack(side='top')
+        tk.Label(master, text="Definition Name", width=50, anchor='w').pack(side='top')
+        def_name_entry = tk.Entry(master, width=50).pack(side='top')
+        tk.Label(master, text="Definition", width=50, anchor='w').pack(side='top')
+        def_entry = tk.Text(master, width=50, height=5).pack(side='top')
+
+        tk.Button(master, text='Enter', command=self.submit, anchor='w').pack(side='top')
+
+
     def down(self, event):
-        print
-        "Click on %s" % event.widget.itemcget(tk.CURRENT, "text")
+        print(
+        "Click on %s" % event.widget.itemcget(tk.CURRENT, "text"))
         self.loc = 1
         self.dragged = 0
         event.widget.bind("<Motion>", self.motion)
@@ -60,17 +67,19 @@ class CanvasDnD(tk.Frame):
     def up(self, event):
         event.widget.unbind("<Motion>")
         if (self.target == event.widget.find_withtag(tk.CURRENT)):
-            print
-            "Select %s" % event.widget.itemcget(tk.CURRENT, "text")
+            print("Select %s" % event.widget.itemcget(tk.CURRENT, "text"))
         else:
             event.widget.itemconfigure(tk.CURRENT, fill="blue")
             self.master.update()
             time.sleep(.1)
-            print
+            print(
             "%s Drag-N-Dropped onto %s" \
             % (event.widget.itemcget(self.target, "text"),
-               event.widget.itemcget(tk.CURRENT, "text"))
+               event.widget.itemcget(tk.CURRENT, "text")))
             event.widget.itemconfigure(tk.CURRENT, fill=self.defaultcolor)
+
+    def submit(self):
+        self.canvas.create_text(75, 75, font=("Helvetica", 14), text="Item 6", tags="DnD")
 
 
 root = tk.Tk()
