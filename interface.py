e340fe2ac2ef337d0c19f1a3156a76eb19147ff1
RIPScollon <scollon@signatureresearchinc.com>
2020-02-06 15:03:00 -0600

    Updating click and definition adding feature.

--- interface.py
+++ interface.py
@@ -4,33 +4,38 @@ import time
 class CanvasDnD(tk.Frame):
     def __init__(self, master):
         self.master = master
-        self.loc = self.dragged = 0
+        self.loc = self.dragged = self.held = 0
         tk.Frame.__init__(self, master)
         canvas = tk.Canvas(self, width=256, height=256,
                            relief=tk.RIDGE, background="white", borderwidth=1)
         self.defaultcolor = canvas.itemcget(
             canvas.create_text(30, 25, font=("Helvetica", 14), text="Item 1", tags="DnD"), "fill")
-        canvas.create_text(75, 75, font=("Helvetica", 14), text="Item 2", tags="DnD")
-        canvas.create_text(125, 125, font=("Helvetica", 14), text="Item 3", tags="DnD")
-        canvas.create_text(175, 175, font=("Helvetica", 14), text="Item 4", tags="DnD")
-        canvas.create_text(225, 225, font=("Helvetica", 14), text="Item 5", tags="DnD")
+
+        self.dictionary = {"Item 1": {"State": "Initial Item"}}
+
         canvas.pack(expand=1, fill=tk.BOTH)
         canvas.tag_bind("DnD", "<ButtonPress-1>", self.down)
         canvas.tag_bind("DnD", "<ButtonRelease-1>", self.chkup)
+
+        canvas.tag_bind("DnD", "<Double-Button-1>", self.define)
+
         canvas.tag_bind("DnD", "<Enter>", self.enter)
         canvas.tag_bind("DnD", "<Leave>", self.leave)
 
         self.canvas = canvas
+        master.bind("<Return>", self.submit)
 
         tk.Label(master, text="Name", width=50, anchor='w').pack(side='top')
-        name_entry = tk.Entry(master, width=50).pack(side='top')
+        self.name_entry = tk.Entry(master, width=50)
+        self.name_entry.pack(side='top')
         tk.Label(master, text="Definition Name", width=50, anchor='w').pack(side='top')
-        def_name_entry = tk.Entry(master, width=50).pack(side='top')
+        self.def_name_entry = tk.Entry(master, width=50)
+        self.def_name_entry.pack(side='top')
         tk.Label(master, text="Definition", width=50, anchor='w').pack(side='top')
-        def_entry = tk.Text(master, width=50, height=5).pack(side='top')
+        self.def_entry = tk.Text(master, width=50, height=5)
+        self.def_entry.pack(side='top')
 
         tk.Button(master, text='Enter', command=self.submit, anchor='w').pack(side='top')
-
 
     def down(self, event):
         print(
@@ -78,8 +83,23 @@ class CanvasDnD(tk.Frame):
                event.widget.itemcget(tk.CURRENT, "text")))
             event.widget.itemconfigure(tk.CURRENT, fill=self.defaultcolor)
 
-    def submit(self):
-        self.canvas.create_text(75, 75, font=("Helvetica", 14), text="Item 6", tags="DnD")
+    def submit(self, *args):
+        name = self.name_entry.get()
+        def_name_entry = self.def_name_entry.get()
+        def_entry = self.def_entry.get("1.0", "end-1c")
+        self.dictionary.update({name: dict})
+        self.dictionary[name].update({def_name_entry: def_entry})
+        self.canvas.create_text(75, 75, font=("Helvetica", 14), text=name, tags="DnD")
+        self.name_entry.delete(0, tk.END)
+        self.def_name_entry.delete(0, tk.END)
+        self.def_entry.delete('1.0', tk.END)
+
+    def define(self, event):
+        self.name_entry.delete(0, tk.END)
+        self.def_name_entry.delete(0, tk.END)
+        self.def_entry.delete('1.0', tk.END)
+        item = event.widget.itemcget(tk.CURRENT, "text")
+        self.name_entry.insert(0, item)
 
 
 root = tk.Tk()
